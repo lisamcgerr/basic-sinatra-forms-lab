5e0ee3b5a308822803062112298555eb69098872
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-01-13 10:02:06 -0600

    commit tree 166d69273a67f3873b06cecc79501ad53fb376c8

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do 
+        erb :newteam
+    end
+
+    post '/team' do
+        binding.pry
+        # @team_name = params[:name]
+        # binding.pry
+        # "hello"
+        # erb :newteam
+    end
 
 end
